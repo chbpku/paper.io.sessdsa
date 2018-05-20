b75509c9d596f3e9c82d86738048955f44843572
YukkuriC <34098703+YukkuriC@users.noreply.github.com>
2018-05-21 01:27:43 +0800

    随机游走AI
    更新接收参数作为示例

--- AI/AI_dumb_random.py
+++ AI/AI_dumb_random.py
@@ -3,6 +3,12 @@ from random import choice
 pool = ['L', 'R'] + [None] * 4
 
 
-def play(*args):
-    '''测试用AI，随机游走'''
+def play(curr_stat, storage):
+    '''
+    测试用AI，随机游走
+    
+    params:
+        curr_stat - 当前游戏状态
+        storage - 可用的缓存字典
+    '''
     return choice(pool)
