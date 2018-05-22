334c32597f694e8dbfa9f77279a4cf8bb459ff6e
zrt0317 <zrt0317@pku.edu.cn>
2018-05-22 15:24:08 +0800

    Add files via upload

---
+++ AI/AI_4x9.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# cb为棋盘
+# ms为函数可以利用的存储字典，其中ms['log']是下棋历史
+
+def play(cb, ms):
+    if not 'stepp' in ms:
+        ms['stepp'] = 1
+    else:
+        ms['stepp'] += 1
+    
+    turnList = ['L', 'S'] # 只会左转或直走
+    if ms['stepp']%11==1 or ms['stepp']%11==4:
+        return turnList[0]
+    else:
+        return turnList[1]
---
+++ AI/AI_6x6.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+# cb为棋盘
+# ms为函数可以利用的存储字典，其中ms['log']是下棋历史
+
+def play(cb, ms):
+    if not 'stepp' in ms:
+        ms['stepp'] = 1
+    else:
+        ms['stepp'] += 1
+    
+    turnList = ['L', 'S'] # 只会左转或直走
+    if ms['stepp']%5==1:
+        return turnList[0]
+    else:
+        return turnList[1]
