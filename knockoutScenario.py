a571a5dd5dab2f3254e5d895567ab1b60a66dbf3
zrt0317 <zrt0317@pku.edu.cn>
2018-06-09 18:13:58 +0800

    细节修改

--- knockoutScenario.py
+++ knockoutScenario.py
@@ -1,9 +1,10 @@
-import os, sys, time, platform, match_core
+import os, sys, platform, match_core
 from prettytable import PrettyTable
 from match_interface import match, save_match_log, clear_storage, swap_storage
 
 
 def knockoutScenario(plrs, FOLDER):    # 单挑赛脚本：读取目录内两个文件，提取play函数并执行
+
     # 屏蔽AI自带print
     class null_stream:
         def read(*args):
@@ -14,14 +15,12 @@ def knockoutScenario(plrs, FOLDER):    # 单挑赛脚本：读取目录内两个
 
         def flush(*args):
             pass
-
-
     sys.stdout = null_stream
-
+    
 
     # 设置比赛参数
     GAMES = 10    # 作为先后手各比赛场数，总场次 2 * GAMES
-    HALFWIDTH, HEIGHT, TIMELIMIT, ROUNDSPERGAME,  = 51, 101, 30, 20
+    HALFWIDTH, HEIGHT, TIMELIMIT, ROUNDSPERGAME = 51, 101, 30, 2000
     CLEAR = 'cls' if platform.system() == 'Windows' else 'clear'    # 设置清屏指令
 
 
@@ -30,7 +29,7 @@ def knockoutScenario(plrs, FOLDER):    # 单挑赛脚本：读取目录内两个
 
 
     # 图形化计分表，比赛结果统计表
-    x = PrettyTable([' #', 'Endgame Winner', 'Game State', '   Rmk   '])
+    x = PrettyTable(['  # ', ' Endgame Winner ', ' Game State ', '    Rmk    '])
     pairResult = [0, 0]
 
 
@@ -58,25 +57,24 @@ def knockoutScenario(plrs, FOLDER):    # 单挑赛脚本：读取目录内两个
 
         # 即时比赛结果的输出文字内容准备
         flag = gameResult[1]
-        if flag < 0:
-            flag += 8
-        rmklst = ['WAL', 'TAP', 'SID', 'FAC', 'CIT', 'END', 'OVT', 'ERR']
-        reason = 'KO'
-        if flag == 3 or flag == 5:
-            reason = str(gameResult[2])
-
+        state = 'KO'
+        if abs(flag) == 3:
+            state = str(gameResult[2])
+        flag = flag + 3
+        rmklst = ['END', 'OVT', 'ERR', 'WAL', 'TAP', 'SID', 'FAC', 'CIT']
+        
         gameRounds = len(match_result['log']) - 1
-        x.add_row(['%2d'%i, winner, reason, rmklst[flag] + ', %4d'%gameRounds])
+        x.add_row(['%2d'%i, winner, state, rmklst[flag] + ', %4d'%gameRounds])
 
         # 更新即时比赛结果
         os.system(CLEAR)
         print(
             name1,
-            ' ' * (17 - len(name1)),
+            ' ' * (21 - len(name1)),
             '%2d' % pairResult[0],
             ': ',
             '%2d' % pairResult[1],
-            ' ' * (19 - len(name2)),
+            ' ' * (23 - len(name2)),
             name2,
             file=sys.__stdout__)
         print(x, file=sys.__stdout__)
@@ -97,25 +95,25 @@ def knockoutScenario(plrs, FOLDER):    # 单挑赛脚本：读取目录内两个
     for i in range(GAMES):    # A vs B 顺序存储空间，先赛GAMES局
         match_core.STORAGE = storageAB
 
-        match_result = match((func1, func2), (name1, name2), HALFWIDTH, HEIGHT,
-                             ROUNDSPERGAME, TIMELIMIT)
-        log_name = '%s/log/%s-VS-%s(%s).zlog' % (FOLDER, name1, name2, 1 + i)
+        match_result = match((func1, func2), (name1, name2), 
+                             HALFWIDTH, HEIGHT, ROUNDSPERGAME, TIMELIMIT)
+        log_name = '%s/log/%s-VS-%s(%s).zlog' % (FOLDER, name1, name2, i)
         save_match_log(match_result, log_name)
 
-        outputer(match_result, i, GAMES, pairResult, name1, name2, x)
+        outputer(match_result, i+1, GAMES, pairResult, name1, name2, x)
 
 
     for i in range(GAMES, GAMES*2):    # B vs A 顺序存储空间，再赛GAMES局    
         match_core.STORAGE = storageBA
 
-        match_result = match((func2, func1), (name2, name1), HALFWIDTH, HEIGHT,
-                             ROUNDSPERGAME, TIMELIMIT)
-        log_name = '%s/log/%s-VS-%s(%s).zlog' % (FOLDER, name2, name1, 1 + i)
+        match_result = match((func2, func1), (name2, name1), 
+                             HALFWIDTH, HEIGHT, ROUNDSPERGAME, TIMELIMIT)
+        log_name = '%s/log/%s-VS-%s(%s).zlog' % (FOLDER, name2, name1, i - GAMES)
         save_match_log(match_result, log_name)
 
-        outputer(match_result, i, GAMES, pairResult, name1, name2, x)
+        outputer(match_result, i+1, GAMES, pairResult, name1, name2, x)
 
-        if pairResult[0] > GAMES or pairResult[1] > GAMES:
+        if abs(pairResult[0]-pairResult[1]) >= 2*GAMES-i:
             break
         
 
@@ -129,7 +127,7 @@ def knockoutScenario(plrs, FOLDER):    # 单挑赛脚本：读取目录内两个
                 pass
 
 
-    # 单挑结束，输出结果；平手的话该对重赛一次
+    # 单挑结束，输出结果
     if pairResult[0] > pairResult[1]:
         totalresult = name1 + ' wins.'
         output = [(name1, func1)], [(name2, func2)]
@@ -139,9 +137,7 @@ def knockoutScenario(plrs, FOLDER):    # 单挑赛脚本：读取目录内两个
     else:
         totalresult = 'Ties.'
         output = [['No one']], None
-    print('Knockout Result:', totalresult, file=sys.__stdout__)
-    time.sleep(5)
-    if output is not None:
-        return output
-    else:
-        return knockout20(plrs, FOLDER)    
+    print('Knockout Result:', totalresult,
+          '\nPress enter to continue:',file=sys.__stdout__)
+    input()
+    return output
