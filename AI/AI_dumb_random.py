b90166fc8a5e6d0d14915e1ce946f13d5660416c
Yukkuri C <799433638@qq.com>
2021-04-19 22:02:52 +0800

    只 送 大 脑

--- AI/AI_dumb_random.py
+++
@@ -1,9 +0,0 @@
-def play(curr_stat, storage):
-    '''随机游走'''
-    return storage['choice']('lrxxxx')
-
-
-def load(stat, storage):
-    '''装载random模块'''
-    from random import choice
-    storage['choice'] = choice
